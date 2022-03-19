10407a114a6eb46d1eee3695d77ea8a7239ad0dd
DelDennis <ddennis.mil@gmail.com>
2022-03-18 17:54:13 -0700

    Save point

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 
 Create S3 bucket + Add versioning
 
+aws s3 sync . s3://eks-gitops
+
 Add github token to secrets manager
-'{{resolve:secretsmanager:/spinnaker/githubtoken:SecretString:GHToken}}'
+'{{resolve:secretsmanager:/throwaway/slightlyunsecured/ghdemotoken:SecretString:GHToken}}'
+
+Build all services and deploy to ECR
+
+aws cloudformation deploy --stack-name EKS-Gitops --template-file ./master.yaml --parameter-overrides file://config/config.json --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM --region us-west-1
